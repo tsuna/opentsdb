cda3871dc98e84dd0d5ff04b9e618d9f6f51f980
Benoit Sigoure <tsuna@stumbleupon.com>
2011-08-09 12:55:57 -0700

    Update hbaseasync from 1.0 to 1.1.0.
    Change-Id: I17f4d7b6f2df884970f7d07af8a1a36b29073ac5

--- third_party/hbase/include.mk
+++ third_party/hbase/include.mk
@@ -13,7 +13,7 @@
 # You should have received a copy of the GNU Lesser General Public License
 # along with this library.  If not, see <http://www.gnu.org/licenses/>.
 
-HBASEASYNC_VERSION := 1.0
+HBASEASYNC_VERSION := 1.1.0
 HBASEASYNC := third_party/hbase/hbaseasync-$(HBASEASYNC_VERSION).jar
 HBASEASYNC_BASE_URL := $(OPENTSDB_THIRD_PARTY_BASE_URL)
 
